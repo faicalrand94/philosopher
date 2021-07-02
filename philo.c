cb0699e6b9e8e6d997bafc10ccbf75dfb35466ca
Faical Bouibaouen <fbouibao@e0r5p8.1337.ma>
2021-07-02 15:45:43 +0100

    98.5%

--- philo.c
+++ philo.c
@@ -6,11 +6,44 @@
 /*   By: fbouibao <fbouibao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 17:01:49 by fbouibao          #+#    #+#             */
-/*   Updated: 2021/06/30 21:15:31 by fbouibao         ###   ########.fr       */
+/*   Updated: 2021/07/02 15:44:43 by fbouibao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int check_arg(char *av)
+{
+	int	i;
+
+	i = -1;
+	if (av[i + 1] == '+')
+		i++;
+	if (av[i + 1] < '0' || av[i + 1] > '9')
+		return (1);
+	while (av[++i])
+	{
+		if (av[i] < '0' || av[i] > '9')
+			return (1);
+	}
+	return (0);
+}
+
+int	check_args(int ac, char *av[])
+{
+	int	i;
+
+	i = 0;
+	if (ac < 5 || ac > 6)
+		return (1);
+	while (av[++i])
+	{
+		if (check_arg(av[i]))
+			return (1);
+	}
+	return (0);
+}
+
 
 int	main(int ac, char *av[])
 {
@@ -19,6 +52,8 @@ int	main(int ac, char *av[])
 
 	if (ac > 4)
 	{
+		if (check_args(ac, av))
+			return (1);
 		ph = initial_ph();
 		get_args(ph, av, ac);
 		loop_creat_thread(ph);
--- treads.c
+++ treads.c
@@ -6,17 +6,29 @@
 /*   By: fbouibao <fbouibao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 17:24:14 by fbouibao          #+#    #+#             */
-/*   Updated: 2021/07/01 13:13:43 by fbouibao         ###   ########.fr       */
+/*   Updated: 2021/07/02 15:25:29 by fbouibao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	ph_cnt(t_philosopher *p)
+{
+	struct timeval	tv;
+	struct timezone	tz;
+
+	ft_print("has taken a fork", p->id, 2, p);
+	p->iseating = 1;
+	ft_print("is eating", p->id, 3, p);
+	(*p->time_each_eat)++;
+	gettimeofday(&tv, NULL);
+	p->last_time_eat = (tv.tv_usec) + (tv.tv_sec * 1000000);
+	mysleep(p->time_to_eat);
+}
+
 void	*philosopher(void *ag)
 {
 	t_philosopher	*p;
-	struct timeval	tv;
-	struct timezone	tz;
 
 	p = (t_philosopher *)ag;
 	p->iseating = 0;
@@ -25,13 +37,7 @@ void	*philosopher(void *ag)
 		pthread_mutex_lock(&p->mut[(p->id - 1)]);
 		ft_print("has taken a fork", p->id, 6, p);
 		pthread_mutex_lock(&p->mut[(p->id) % p->nbr_ph]);
-		ft_print("has taken a fork", p->id, 2, p);
-		p->iseating = 1;
-		ft_print("is eating", p->id, 3, p);
-		(*p->time_each_eat)++;
-		gettimeofday(&tv, NULL);
-		p->last_time_eat = (tv.tv_usec) + (tv.tv_sec * 1000000);
-		mysleep(p->time_to_eat);
+		ph_cnt(p);
 		pthread_mutex_unlock(&p->mut[(p->id) % p->nbr_ph]);
 		pthread_mutex_unlock(&p->mut[(p->id - 1)]);
 		ft_print("is sleeping", p->id, 4, p);
